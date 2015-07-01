afb2e4f0b1f70857df20f387196aab65a0beb5a4
Markus <markus@creativeandcommercial.co.uk>
2015-07-01 14:19:24 +0100

    Chai homepage front end build

---
+++ home-template/js/map-config.js
@@ -0,0 +1,115 @@
+﻿var map_config = {
+	'default':{
+		'borderColor':'transparent', //inter-region borders
+		'mapShadow':'transparent', //shadow color below the map
+		'shadowOpacity':'0', //shadow opacity, value, 0-100
+		'hoverShadow':'transparent', //tooltip shadow color
+	},
+	'map_1':{
+		'hover': 'EAST ANGLIA',  //region hover content
+		'url':'http://www.html5interactivemaps.com/', //Goto URL
+		'target':'same_window', //open link in new window:new_window, open in current window:same_window, or none for nothing.
+		'upColor':'#982061', //region's color when page loads
+		'overColor':'#982061', //region's color when mouse hover
+		'downColor':'#982061',//region's color when mouse clicking
+		'enable':true,//true/false to enable/disable this region
+	},
+	'map_2':{
+		'hover': 'EAST MIDLANDS',
+		'url':'http://www.html5interactivemaps.com/',
+		'target':'same_window',
+		'upColor':'#982061',
+		'overColor':'#982061',
+		'downColor':'#982061',
+		'enable':true,
+	},
+	'map_3':{
+		'hover': 'LONDON',
+		'url':'http://www.html5interactivemaps.com/',
+		'target':'same_window',
+		'upColor':'#982061',
+		'overColor':'#982061',
+		'downColor':'#982061',
+		'enable':true,
+	},
+	'map_4':{
+		'hover': 'NORTH EAST',
+		'url':'http://www.html5interactivemaps.com/',
+		'target':'same_window',
+		'upColor':'#982061',
+		'overColor':'#982061',
+		'downColor':'#982061',
+		'enable':true,
+	},
+	'map_5':{
+		'hover': 'NORTHERN IRELAND',
+		'url':'http://www.html5interactivemaps.com/',
+		'target':'same_window',
+		'upColor':'#982061',
+		'overColor':'#982061',
+		'downColor':'#982061',
+		'enable':true,
+	},
+	'map_6':{
+		'hover': 'NORTH WEST',
+		'url':'http://www.html5interactivemaps.com/',
+		'target':'same_window',
+		'upColor':'#982061',
+		'overColor':'#982061',
+		'downColor':'#982061',
+		'enable':true,
+	},
+	'map_7':{
+		'hover': 'SCOTLAND',
+		'url':'http://www.html5interactivemaps.com/',
+		'target':'same_window',
+		'upColor':'#982061',
+		'overColor':'#982061',
+		'downColor':'#982061',
+		'enable':true,
+	},
+	'map_8':{
+		'hover': 'SOUTH EAST',
+		'url':'http://www.html5interactivemaps.com/',
+		'target':'same_window',
+		'upColor':'#982061',
+		'overColor':'#982061',
+		'downColor':'#982061',
+		'enable':true,
+	},
+	'map_9':{
+		'hover': 'SOUTH WEST',
+		'url':'http://www.html5interactivemaps.com/',
+		'target':'same_window',
+		'upColor':'#982061',
+		'overColor':'#982061',
+		'downColor':'#982061',
+		'enable':true,
+	},
+	'map_10':{
+		'hover': 'WALES',
+		'url':'http://www.html5interactivemaps.com/',
+		'target':'same_window',
+		'upColor':'#982061',
+		'overColor':'#982061',
+		'downColor':'#982061',
+		'enable':true,
+	},
+	'map_11':{
+		'hover': 'WEST MIDLANDS',
+		'url':'http://www.html5interactivemaps.com/',
+		'target':'same_window',
+		'upColor':'#982061',
+		'overColor':'#982061',
+		'downColor':'#982061',
+		'enable':true,
+	},
+	'map_12':{
+		'hover': 'YORKSHIRE',
+		'url':'http://www.html5interactivemaps.com/',
+		'target':'same_window',
+		'upColor':'#982061',
+		'overColor':'#982061',
+		'enable':true,
+	},
+}
---
+++ home-template/js/pin-config.js
@@ -0,0 +1,174 @@
+﻿var pin_config = {
+	'default':{
+		'pinShadow':'#000', //shadow color below the points
+		'pinShadowOpacity':'50', //shadow opacity, value, 0-100
+	},
+	'points':[
+	{
+		'shape':'circle',//choose the shape of the pin circle or rectangle
+		'hover': '<h3>London</h3><br>Walton Community Centre<br>May Terrace,<br>Glasgow<br>G46 6LD<br>Freephone helpline<br>0808 808 4567<br>Reception 0208 202 2211',//the content of the hover popup
+		'pos_X':279,//location of the pin on X axis
+		'pos_Y':415,//location of the pin on Y axis
+		'diameter':8,
+		'outline':'transparent',//outline color of the pin
+		'thickness':1,//thickness of the line (0 to hide the line)
+		'upColor':'#ff9900',//color of the pin when map loads
+		'overColor':'#2d358e',//color of the pin when mouse hover
+		'downColor':'#00ffff',//color of the pin when clicked 
+		//(trick, if you make this pin un-clickable > make the overColor and downColor the same)
+		'url':'http://www.html5interactivemaps.com',//URL of this pin
+		'target':'new_window',//'new_window' opens URL in new window//'same_window' opens URL in the same window //'none' pin is not clickable
+		'enable':true,//true/false to enable/disable this pin
+	},
+	{
+		'shape':'circle',
+		'hover': '<h3>Birmingham</h3><br>Walton Community Centre<br>May Terrace,<br>Glasgow<br>G46 6LD<br>Freephone helpline<br>0808 808 4567<br>Reception 0208 202 2211',//the content of the hover popup
+		'pos_X':225,
+		'pos_Y':362,
+		'diameter':8,
+		'outline':'transparent',
+		'thickness':1,
+		'upColor':'#ff9900',
+		'overColor':'#2d358e',
+		'downColor':'#00ffff',
+		'url':'http://www.html5interactivemaps.com',
+		'target':'same_window',
+		'enable':true,
+	},
+	{
+		'shape':'circle',
+		'hover': '<h3>Manchester</h3><br>Walton Community Centre<br>May Terrace,<br>Glasgow<br>G46 6LD<br>Freephone helpline<br>0808 808 4567<br>Reception 0208 202 2211',//the content of the hover popup
+		'pos_X':209,
+		'pos_Y':300,
+		'diameter':8,
+		'outline':'transparent',
+		'thickness':1,
+		'upColor':'#ff9900',
+		'overColor':'#2d358e',
+		'downColor':'#00ffff',
+		'url':'http://www.html5interactivemaps.com',
+		'target':'same_window',
+		'enable':true,
+	},
+	{
+		'shape':'circle',
+		'hover': '<h3>Liverpool</h3><br>Walton Community Centre<br>May Terrace,<br>Glasgow<br>G46 6LD<br>Freephone helpline<br>0808 808 4567<br>Reception 0208 202 2211',//the content of the hover popup
+		'pos_X':187,
+		'pos_Y':304,
+		'diameter':8,
+		'outline':'transparent',
+		'thickness':1,
+		'upColor':'#ff9900',
+		'overColor':'#2d358e',
+		'downColor':'#ff9900',
+		'url':'http://www.html5interactivemaps.com',
+		'target':'none',
+		'enable':true,
+	},
+	{
+		'shape':'circle',
+		'hover': '<h3>Leeds</h3><br>Walton Community Centre<br>May Terrace,<br>Glasgow<br>G46 6LD<br>Freephone helpline<br>0808 808 4567<br>Reception 0208 202 2211',//the content of the hover popup
+		'pos_X':237,
+		'pos_Y':282,
+		'diameter':8,
+		'outline':'transparent',
+		'thickness':1,
+		'upColor':'#ff9900',
+		'overColor':'#2d358e',
+		'downColor':'#00ffff',
+		'url':'http://www.html5interactivemaps.com',
+		'target':'same_window',
+		'enable':true,
+	},
+	{
+		'shape':'circle',
+		'hover': '<h3>Sheffield</h3><br>Walton Community Centre<br>May Terrace,<br>Glasgow<br>G46 6LD<br>Freephone helpline<br>0808 808 4567<br>Reception 0208 202 2211',//the content of the hover popup
+		'pos_X':237,
+		'pos_Y':304,
+		'diameter':8,
+		'outline':'transparent',
+		'thickness':1,
+		'upColor':'#ff9900',
+		'overColor':'#2d358e',
+		'downColor':'#00ffff',
+		'url':'http://www.html5interactivemaps.com',
+		'target':'same_window',
+		'enable':true,
+	},
+	{
+		'shape':'circle',
+		'hover': '<h3>Bristol</h3><br>Walton Community Centre<br>May Terrace,<br>Glasgow<br>G46 6LD<br>Freephone helpline<br>0808 808 4567<br>Reception 0208 202 2211',//the content of the hover popup
+		'pos_X':196,
+		'pos_Y':412,
+		'diameter':8,
+		'outline':'transparent',
+		'thickness':1,
+		'upColor':'#ff9900',
+		'overColor':'#2d358e',
+		'downColor':'#00ffff',
+		'url':'http://www.html5interactivemaps.com',
+		'target':'same_window',
+		'enable':true,
+	},
+	{
+		'shape':'circle',
+		'hover': '<h3>Stoke-on-Trent</h3><br>Walton Community Centre<br>May Terrace,<br>Glasgow<br>G46 6LD<br>Freephone helpline<br>0808 808 4567<br>Reception 0208 202 2211',//the content of the hover popup
+		'pos_X':211,
+		'pos_Y':331,
+		'diameter':8,
+		'outline':'transparent',
+		'thickness':1,
+		'upColor':'#ff9900',
+		'overColor':'#2d358e',
+		'downColor':'#00ffff',
+		'url':'http://www.html5interactivemaps.com',
+		'target':'same_window',
+		'enable':true,
+	},
+	{
+		'shape':'circle',
+		'hover': '<h3>Leicester</h3><br>Walton Community Centre<br>May Terrace,<br>Glasgow<br>G46 6LD<br>Freephone helpline<br>0808 808 4567<br>Reception 0208 202 2211',//the content of the hover popup
+		'pos_X':248,
+		'pos_Y':348,
+		'diameter':8,
+		'outline':'transparent',
+		'thickness':1,
+		'upColor':'#ff9900',
+		'overColor':'#2d358e',
+		'downColor':'#00ffff',
+		'url':'http://www.html5interactivemaps.com',
+		'target':'same_window',
+		'enable':true,
+	},
+	{
+		'shape':'circle',
+		'hover': '<h3>Edinburgh</h3><br>Walton Community Centre<br>May Terrace,<br>Glasgow<br>G46 6LD<br>Freephone helpline<br>0808 808 4567<br>Reception 0208 202 2211',//the content of the hover popup
+		'pos_X':185,
+		'pos_Y':158,
+		'diameter':8,
+		'outline':'transparent',
+		'thickness':1,
+		'upColor':'#ff9900',
+		'overColor':'#2d358e',
+		'downColor':'#00ffff',
+		'url':'http://www.html5interactivemaps.com',
+		'target':'same_window',
+		'enable':true,
+	},
+	{
+		'shape':'circle',
+		'hover': '<h3>Belfast</h3>s<br>Walton Community Centre<br>May Terrace,<br>Glasgow<br>G46 6LD<br>Freephone helpline<br>0808 808 4567<br>Reception 0208 202 2211',//the content of the hover popup
+		'pos_X':85,
+		'pos_Y':228,
+		'diameter':8,
+		'outline':'transparent',
+		'thickness':1,
+		'upColor':'#ff9900',
+		'overColor':'#2d358e',
+		'downColor':'#00ffff',
+		'url':'http://www.html5interactivemaps.com',
+		'target':'same_window',
+		'enable':true,
+	},
+	]
+}
